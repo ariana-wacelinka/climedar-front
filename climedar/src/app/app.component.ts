801ccf9df3234031e64f6d80eb77561e8cfe7a89
IgnacioL24 <ignaciolances24@gmail.com>
2025-01-20 19:53:31 -0300

    Añadido la paleta de colores a material

--- climedar/src/app/app.component.ts
+++ climedar/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
