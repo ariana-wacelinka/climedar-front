4ba54e3c673b0fd4b874e0d657341c338356d780
IgnacioL24 <ignaciolances24@gmail.com>
2025-01-21 18:08:36 -0300

    agregado formgroup

--- climedar/src/app/auth/login/login.component.ts
+++ climedar/src/app/auth/login/login.component.ts
@@ -4,6 +4,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   imports: [
@@ -11,11 +12,17 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  formgroup: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.email),
+    password: new FormControl('')
+  });
 
 }
