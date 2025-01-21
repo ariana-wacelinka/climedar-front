5ec840ba8db5f8c672ffc846b223db68aefba598
IgnacioL24 <ignaciolances24@gmail.com>
2025-01-21 18:00:55 -0300

    cambios en estilos

--- climedar/src/app/auth/login/login.component.ts
+++ climedar/src/app/auth/login/login.component.ts
@@ -3,14 +3,15 @@ import { CenteredCardComponent } from '../../shared/components';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-login',
   imports: [
     CenteredCardComponent,
     MatInputModule,
     MatFormFieldModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
