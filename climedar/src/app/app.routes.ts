8bc53384103724cf9cc57f18a03c70e9fa69e0e6
IgnacioL24 <ignaciolances24@gmail.com>
2025-01-21 17:36:40 -0300

    ejemplo de login utilizando la card

--- climedar/src/app/app.routes.ts
+++ climedar/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CenteredCardComponent } from './shared/components/centered-card/centered-card.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: CenteredCardComponent},
-    {path: 'home', component: CenteredCardComponent}
+    {path: 'home', component: CenteredCardComponent},
+    {path: 'login', component: LoginComponent}
 ];
--- climedar/src/app/auth/login/login.component.ts
+++ climedar/src/app/auth/login/login.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { CenteredCardComponent } from '../../shared/components';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CenteredCardComponent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
