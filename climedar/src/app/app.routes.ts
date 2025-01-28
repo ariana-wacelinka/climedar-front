c4211b5c5f778f05f2b86918ee3b82313379785c
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-01-28 13:01:00 -0300

    Tabla con filter hecho

--- climedar/src/app/app.routes.ts
+++ climedar/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CenteredCardComponent } from './shared/components/centered-card/centered-card.component';
 import { LoginComponent } from './auth/login/login.component';
 import { AltaDoctorComponent } from './doctors/alta-doctor/alta-doctor.component';
+import {ObraSocialComponent} from './obra-social/obra-social.component';
 
 export const routes: Routes = [
     {path: '', component: CenteredCardComponent},
     {path: 'home', component: CenteredCardComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'doctor/nuevo', component: AltaDoctorComponent}
+    {path: 'doctor/nuevo', component: AltaDoctorComponent},
+    {path: 'obra-social/listado', component: ObraSocialComponent}
 ];
---
+++ climedar/src/app/obra-social/obra-social.component.ts
@@ -0,0 +1,75 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {CenteredCardComponent} from '../shared/components';
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable,
+  MatTableDataSource, MatTableModule
+} from '@angular/material/table';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatIcon} from '@angular/material/icon';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
+// noinspection AngularUnusedComponentImport
+@Component({
+  selector: 'app-obra-social',
+  imports: [
+    CenteredCardComponent,
+    MatTable,
+    MatTableModule,
+    MatColumnDef,
+    MatPaginatorModule,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatIcon,
+    MatPaginator,
+    MatSort,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
+  ],
+  templateUrl: './obra-social.component.html',
+  styleUrl: './obra-social.component.scss'
+})
+export class ObraSocialComponent implements AfterViewInit {
+
+  @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator;
+  @ViewChild(MatSort) sort: MatSort = new MatSort;
+
+  displayedColumns: string[] = ['nombre'];
+  dataSource = new MatTableDataSource([
+    {nombre: 'OSPE'},
+    {nombre: 'OSDE'},
+    {nombre: 'PAMI'},
+    {nombre: 'IOMA'},
+    {nombre: 'OSECAC'},
+  ]);
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
