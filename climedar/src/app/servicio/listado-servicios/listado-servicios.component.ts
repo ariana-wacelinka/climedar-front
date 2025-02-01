8d15d5719d23cc5ea7c88c03a0c6f65ba87df8a7
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-02-01 13:32:13 -0300

    Creacion del componente listado de servicios

---
+++ climedar/src/app/servicio/listado-servicios/listado-servicios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-servicios',
+  imports: [],
+  templateUrl: './listado-servicios.component.html',
+  styleUrl: './listado-servicios.component.scss'
+})
+export class ListadoServiciosComponent {
+
+}
