eeb77813f2fc5b66152be7156810ee17fe9719df
IgnacioL24 <ignaciolances24@gmail.com>
2025-02-06 19:13:35 -0300

    fix de paginador

--- climedar/src/app/shared/components/paginator/paginator.component.ts
+++ climedar/src/app/shared/components/paginator/paginator.component.ts
@@ -10,8 +10,6 @@ import {MatRipple} from '@angular/material/core';
   styleUrls: ['./paginator.component.scss']
 })
 export class PaginatorComponent {
-  public disabled1 = signal(true);
-  public disabled2 = signal(false);
 
   @Input() totalItems = 0;
   @Input() pageSize = 0;
@@ -20,34 +18,24 @@ export class PaginatorComponent {
 
   public currentPage = signal(1);
 
-  get previousPage(): number {
-    return Math.max(1, this.currentPage() - 1);
-  }
-
-  get followingPage(): number {
-    return Math.min(this.pages, this.currentPage() + 1);
-  }
-
   public get pages(): number {
     return Math.ceil(this.totalItems / this.pageSize);
   }
 
+  disabled1() {
+    return this.currentPage() === 1;
+  }
+
+  disabled2() {
+    return this.currentPage() === this.pages;
+  }
+
   movePreviousPage() {
-    if (this.currentPage() === 2) {
-      this.disabled1.set(true);
-    } 
-    if (this.totalItems/this.pageSize > 1) {
-      this.disabled2.set(false);
-    }
     this.currentPage.set(this.currentPage() - 1);
     this.pageChange.emit(this.currentPage());
   }
 
   moveFollowingPage() {
-    if (Math.floor(this.totalItems / this.pageSize) === this.currentPage()) {
-      this.disabled2.set(true);
-    } 
-    this.disabled1.set(false);
     this.currentPage.set(this.currentPage() + 1);
     this.pageChange.emit(this.currentPage());
   }
