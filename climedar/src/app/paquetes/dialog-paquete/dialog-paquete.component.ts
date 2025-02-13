037259731645ffc5076d7eee4df1097c5da667ec
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-02-13 20:57:28 -0300

    json listo para hacer el create de paquete

--- climedar/src/app/paquetes/dialog-paquete/dialog-paquete.component.ts
+++ climedar/src/app/paquetes/dialog-paquete/dialog-paquete.component.ts
@@ -1,5 +1,5 @@
-import {Component, Inject, model, OnInit} from '@angular/core';
-import {MatButton} from "@angular/material/button";
+import { Component, Inject, signal, WritableSignal } from '@angular/core';
+import { MatButton } from "@angular/material/button";
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -7,30 +7,19 @@ import {
   MatDialogRef,
   MatDialogTitle
 } from "@angular/material/dialog";
-import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell, MatHeaderCellDef,
-  MatHeaderRow, MatHeaderRowDef, MatNoDataRow, MatRow, MatRowDef, MatTable,
-  MatTableDataSource
-} from '@angular/material/table';
-import {MatIcon} from '@angular/material/icon';
-import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
-import {MatSort, MatSortHeader} from '@angular/material/sort';
-import {MatDivider} from '@angular/material/divider';
-import {MatFormField, MatLabel, MatPrefix} from '@angular/material/form-field';
-import {MatInput} from '@angular/material/input';
-import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatCheckbox} from '@angular/material/checkbox';
-
-interface Servicio {
-  id: number;
-  nombre: string;
-  descripcion: string;
-  precio: number;
-  duracionEstimada: number;
-}
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { PaginatorComponent } from "../../shared/components/paginator/paginator.component";
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule, MatSelectionList } from '@angular/material/list';
+import { PageInfo } from '../../shared/models/extras.models';
+import { MedicalService } from '../../servicio/models/services.models';
+import { ServiciosMedicosService } from '../../servicio/services/servicio/servicios-medicos.service';
+import { Duration } from 'luxon';
+import { CurrencyPipe } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-dialog-paquete',
@@ -40,103 +29,121 @@ interface Servicio {
     MatDialogActions,
     MatDialogContent,
     MatDialogTitle,
-    MatCell,
-    MatCellDef,
-    MatColumnDef,
-    MatHeaderCell,
-    MatHeaderRow,
-    MatHeaderRowDef,
     MatIcon,
-    MatRow,
-    MatRowDef,
-    MatTable,
-    MatHeaderCellDef,
     MatFormField,
     MatInput,
     MatLabel,
-    MatPrefix,
     FormsModule,
     ReactiveFormsModule,
-    MatCheckbox
+    PaginatorComponent,
+    MatCardModule,
+    MatSelectionList,
+    MatListModule,
+    CurrencyPipe,
+    MatTabsModule
   ],
   styleUrls: ['./dialog-paquete.component.scss']
 })
 export class DialogPaqueteComponent {
-  readonly indeterminate = false;
-  selectedServices = new Set<number>();
+  servicios = signal<MedicalService[]>([]);
+  totalAmount = signal<number>(0);
+  pageInfo = signal<PageInfo>({ totalItems: 0, currentPage: 1, totalPages: 0 });
+  displayedColumns: string[] = ["select", "nombre", "precio"];
+  servicioControl = new FormControl<string>("");
 
-  displayedColumns: string[] = ["select", "nombre", "precio", "duracionEstimada"];
-  dataSource = new MatTableDataSource([
-    {nombre: 'Consulta General', descripcion: 'Consulta médica general', precio: 500, duracionEstimada: '00:30', id: 0},
-    {nombre: 'Radiografía', descripcion: 'Radiografía de cualquier parte del cuerpo', precio: 800, duracionEstimada: '00:30', id: 1},
-    {nombre: 'Análisis de Sangre', descripcion: 'Análisis completo de sangre', precio: 300, duracionEstimada: '00:30', id: 2},
-    {nombre: 'Ecografía', descripcion: 'Ecografía de cualquier parte del cuerpo', precio: 1000, duracionEstimada: '01:00', id: 3},
-    {nombre: 'Fisioterapia', descripcion: 'Sesión de fisioterapia', precio: 600, duracionEstimada: '01:00', id: 4}
-  ]);
-
-  formGroup: FormGroup;
+  paquete = new FormGroup({
+    id: new FormControl<string>(""),
+    nombre: new FormControl<string>("", Validators.required),
+    medicalServices: new FormControl<string[]>([])
+  })
 
   constructor(public dialogRef: MatDialogRef<DialogPaqueteComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: { id?: number, nombre?: string, services?:Servicio[]},
-              private fb: FormBuilder
-  ) {
-    this.formGroup = this.fb.group({
-      nombre: [data.nombre || '', Validators.required],
-      servicios: this.fb.array(data.services ? data.services.map(service => this.fb.group(service)) : [])
-    });
-
-    this.initializeSelectedServices();
-  }
-  
-  initializeSelectedServices() {
-    if (this.data.services) {
-      this.data.services.forEach(service => {
-        this.selectedServices.add(service.id);
-      });
+    private medicalService: ServiciosMedicosService,
+    @Inject(MAT_DIALOG_DATA) public data: {
+      id?: number,
+      nombre?: string,
+      medicalServices?: MedicalService[]
+    }) {
+    if (data.id) {
+      this.paquete.controls.id.setValue(data.id?.toString());
+      this.paquete.controls.nombre.setValue(data.nombre!);
+      this.paquete.controls.medicalServices.setValue(data.medicalServices!.map(service => service.id!));
     }
   }
 
-  trackById(index: number, item: Servicio) {
-    return item.id;
+  ngOnInit() {
+    this.getServices();
   }
-  
+
+  getServices(): void {
+    this.medicalService.getAllServiciosMedicos(this.pageInfo().currentPage).subscribe((data) => {
+      this.servicios.set(data.services);
+      this.pageInfo.set(data.pageInfo);
+    });
+  }
+
+  formatDurationTime(duration: string): string {
+    return Duration.fromISO(duration).as('minutes') + ' min';
+  }
+
+  currentPage(): WritableSignal<number> {
+    return signal<number>(this.pageInfo().currentPage + 1);
+  }
+
+  pageChange(page: number) {
+    this.medicalService.getServiciosMedicos(this.servicioControl.value ?? "", "", page).subscribe((response) => {
+      this.servicios.set(response.services);
+      this.pageInfo.set(response.pageInfo);
+    });
+  }
+
+  selectionChange(event: boolean, id: string) {
+    const selectedServices = this.paquete.controls.medicalServices.value || [];
+
+    if (event) {
+      this.paquete.controls.medicalServices.setValue([...selectedServices, id]);
+    } else {
+      this.paquete.controls.medicalServices.setValue(selectedServices.filter(serviceId => serviceId !== id));
+    }
+
+    const servicio = this.servicios().find(servicio => servicio.id === id);
+    if (servicio) {
+      this.totalAmount.set(event ? this.totalAmount() + Number(servicio.price) : this.totalAmount() - Number(servicio.price));
+    }
+  }
+
+  isServicioSelected(id: string): boolean {
+    return this.paquete.controls.medicalServices.value!.includes(id);
+  }
+
   onClose() {
     this.dialogRef.close();
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
   onSubmit() {
-    if (this.data.id == null){
-      if (this.formGroup.valid){
-        let i = 0;
-        alert('Paquete creado: ' + this.formGroup.value.nombre);
-        for (let id of this.selectedServices){
-          console.log('Servicio ' + i + ': ' + id);
-        }
+    if (this.data.id == null) {
+      if (this.paquete.valid) {
+        const paquete = {
+          nombre: this.paquete.value.nombre,
+          medicalServices: this.paquete.value.medicalServices
+        };
+
+        console.log(paquete);
+        alert('Paquete creado' + this.paquete.value);
         this.onClose();
       }
     } else {
-      if (this.formGroup.valid){
-        let i = 0;
-        alert('Paquete editado: ' + this.formGroup.value.nombre);
-        for (let id of this.selectedServices){
-          console.log('Servicio ' + i + ': ' + id);
-        }
+      if (this.paquete.valid) {
+        const paquete = {
+          id: this.paquete.value.id,
+          nombre: this.paquete.value.nombre,
+          medicalServices: this.paquete.value.medicalServices
+        };
+        
+        console.log(paquete);
+        alert('Paquete editado: ' + this.paquete.value);
         this.onClose();
       }
     }
   }
-
-  toggleSelection(id: number, isChecked: boolean) {
-    if (isChecked) {
-      this.selectedServices.add(id);
-      console.log(Array.from(this.selectedServices));
-    } else {
-      this.selectedServices.delete(id);
-    }
-  }  
 }
