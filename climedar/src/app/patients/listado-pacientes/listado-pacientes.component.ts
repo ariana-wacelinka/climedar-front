9310bcfe614b1c4e39694650a5e64a8df7993f78
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-02-14 18:41:38 -0300

    Listado de pacientes creado

---
+++ climedar/src/app/patients/listado-pacientes/listado-pacientes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-pacientes',
+  imports: [],
+  templateUrl: './listado-pacientes.component.html',
+  styleUrl: './listado-pacientes.component.scss'
+})
+export class ListadoPacientesComponent {
+
+}
