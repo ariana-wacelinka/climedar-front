2447173924d92166c6f0ec196d531047a96279d5
IgnacioL24 <ignaciolances24@gmail.com>
2025-02-23 04:11:35 -0300

    fix actions

--- climedar/prebuild.js
+++ climedar/prebuild.js
@@ -1,35 +1,20 @@
 const fs = require('fs');
-const { exec } = require('child_process');
 
-// Ejecuta el comando para obtener las variables de Firebase
-exec('firebase functions:config:get', (err, stdout) => {
-  if (err) {
-    console.error('❌ Error al obtener las variables de Firebase:', err);
-    return;
-  }
-
-  // Parsear la respuesta JSON
-  const firebaseConfig = JSON.parse(stdout);
-
-  // Generar el archivo environment.prod.ts con las variables
-  const envConfig = `
+// Obtén las variables de entorno de GitHub Actions
+const envConfig = `
 export const environment = {
   production: true,
   auth0: {
-    domain: '${firebaseConfig.auth0.domain}',
-    clientId: '${firebaseConfig.auth0.clientid}',
-    database: '${firebaseConfig.auth0.database}',
-    audience: '${firebaseConfig.auth0.audience}'
+    domain: '${process.env.AUTH0_DOMAIN}',
+    clientId: '${process.env.AUTH0_CLIENT_ID}',
+    database: '${process.env.AUTH0_DATABASE}',
+    audience: '${process.env.AUTH0_AUDIENCE}'
   },
-  apiUrl: '${firebaseConfig.api.url}',
-  paymentUrl: '${firebaseConfig.payment.url}'
+  apiUrl: '${process.env.API_URL}',
+  paymentUrl: '${process.env.PAYMENT_URL}'
 };
 `;
 
-if (!fs.existsSync('src/environments/environment.prod.ts')) {
-    fs.writeFileSync('src/environments/environment.prod.ts', envConfig);
-    console.log('✅ environment.prod.ts generado con variables de Firebase');
-} else {
-    console.log('❌ environment.prod.ts ya existe');
-}
-});
+// Escribe el archivo environment.prod.ts
+fs.writeFileSync('src/environments/environment.prod.ts', envConfig);
+console.log('✅ environment.prod.ts generado con variables de entorno');
