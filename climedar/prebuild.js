e698fc11268cb5c3e854d469176c11ca638a2f04
IgnacioL24 <ignaciolances24@gmail.com>
2025-02-23 04:17:59 -0300

    fix

--- climedar/prebuild.js
+++ climedar/prebuild.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Obtén la ruta absoluta al directorio environments
-const envDir = path.join(__dirname, 'src/environments');
+const envDir = path.join(__dirname, 'src/app/environments');
 const envFile = path.join(envDir, 'environment.prod.ts');
 
 // Crea el directorio si no existe
