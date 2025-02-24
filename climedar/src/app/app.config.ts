4fd2d595d212764356769f268e01b146c6da0976
IgnacioL24 <ignaciolances24@gmail.com>
2025-02-23 22:18:45 -0300

    envio de token

--- climedar/src/app/app.config.ts
+++ climedar/src/app/app.config.ts
@@ -13,7 +13,7 @@ import { LOCALE_ID, inject } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { ApolloLink, InMemoryCache } from '@apollo/client/core';
+import { ApolloLink, InMemoryCache, Observable } from '@apollo/client/core';
 import { AuthService } from './auth/service/auth.service';
 
 registerLocaleData(localeEs, 'es');
@@ -37,27 +37,28 @@ export const appConfig: ApplicationConfig = {
     provideApollo(() => {
       const httpLink = inject(HttpLink);
       const auth = inject(AuthService);
-      console.log('entra a appConfig para peticiones');
-      // TODO: DESCOMENTAR CUANDO TENGAMOS QUE ENVIAR EL TOKEN EN CADA REQUEST
+
       // Middleware para agregar el token automáticamente a cada request
       const authLink = new ApolloLink((operation, forward) => {
-        const token = auth.getToken(); // Obtiene el token de localStorage
-        console.log('token', token);
-        operation.setContext({
-          headers: {
-            Authorization: token ? `Bearer ${token}` : '', // Agrega el token al header
-          },
+        return new Observable(observer => {
+          const token = auth.getToken(); // Obtiene el token
+          console.log('Token:', token);
+
+          operation.setContext({
+            headers: {
+              Authorization: token ? `Bearer ${token}` : ''
+            }
+          });
+
+          // Continuar con la request
+          forward(operation).subscribe(observer);
         });
-
-        console.log('operation', operation);
-
-        return forward(operation);
       });
 
       return {
-        link: httpLink.create({
+        link: authLink.concat(httpLink.create({
           uri: environment.apiUrl,
-        }),
+        })),
         cache: new InMemoryCache(),
       };
     })]
